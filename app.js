78f8dede6cdcf1b6c53a052d1fe05dd6ed2216c7
Babs Craig <babscraig@gmail.com>
2017-02-26 00:23:20 +0100

    added functionality to pull in blog post from MongoDB using monk

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+var multer  = require('multer');
+var upload = multer({ dest: './public/images/uploads' });
+var flash = require('connect-flash');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.locals.moment = require('moment');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,8 +27,54 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Handle express sessions
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+//Passport
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+//Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Flash messages set up
+app.use(flash());
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Set up db and make accessible to router
+app.use(function(req,res,next) {
+  req.db = db;
+  next();
+})
+
+//Set up routes
 app.use('/', index);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
 
-/* GET home page. */
+
+
+// Home page blog posts
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var db = req.db;
+  var posts = db.get('posts');
+  posts.find({}, function(err, posts) {
+    res.render('index', {
+      posts: posts
+    });
+  })
 });
 
 module.exports = router;
